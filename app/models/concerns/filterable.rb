f1bc08cd900c52190827ad5db32daaeb3878681d
Dan Milne <d@nmilne.com>
2026-02-28 00:21:08 +1100

    Revert Filterable to use ClassMethods with all instead of where(nil)

--- app/models/concerns/filterable.rb
+++ app/models/concerns/filterable.rb
@@ -4,15 +4,26 @@
 module Filterable
   extend ActiveSupport::Concern
 
-  included do
-    def self.filter(params)
+  module ClassMethods
+
+    # Call the class methods with the same name as the keys in <tt>filtering_params</tt>
+    # with their associated values. Most useful for calling named scopes from
+    # URL params. Make sure you don't pass stuff directly from the web without
+    # whitelisting only the params you care about first!
+    def filter(params)
       params = params.stringify_keys
       results = all
       params.each do |key, value|
         if value.present? || (value.is_a?(TrueClass) || value.is_a?(FalseClass))
-          value = value.split(",") if value.is_a? String
+          value = value.split(',') if value.is_a? String
           results = results.public_send(key, value)
         end
+      end
+      results
+    end
+  end
+end
+
       end
       results
     end
