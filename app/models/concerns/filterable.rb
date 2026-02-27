0f9df8c0c69793e05bb51c39e0854f648eb7bbf1
Dan Milne <d@nmilne.com>
2026-02-28 00:16:42 +1100

    Fix Filterable#filter to work on relations
    Define filter as class method inside included block so it works on both
    Scene.filter() and @scenes.filter() without conflicting with
    Enumerable#filter

--- app/models/concerns/filterable.rb
+++ app/models/concerns/filterable.rb
@@ -4,20 +4,20 @@
 module Filterable
   extend ActiveSupport::Concern
 
-  module ClassMethods
-
-    # Call the class methods with the same name as the keys in <tt>filtering_params</tt>
-    # with their associated values. Most useful for calling named scopes from
-    # URL params. Make sure you don't pass stuff directly from the web without
-    # whitelisting only the params you care about first!
-    def filter(params)
+  included do
+    def self.filter(params)
       params = params.stringify_keys
-      results = self.where(nil) # create an anonymous scope
+      results = all
       params.each do |key, value|
-        if value.present? || (value.is_a?(TrueClass) || value.is_a?(FalseClass)) # TODO: Added this boolean check, test this...
+        if value.present? || (value.is_a?(TrueClass) || value.is_a?(FalseClass))
           value = value.split(',') if value.is_a? String
           results = results.public_send(key, value)
         end
+      end
+      results
+    end
+  end
+end
       end
       results
     end
